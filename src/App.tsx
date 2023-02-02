c0961db641cab1a35255f3df62e9a5ed203b5375
Tomiwa Ademidun <tomiademidun@gmail.com>
2023-02-02 07:54:37 +0200

    add react-router-dom and Products List scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductsList from "./scenes/Products/ProductsList";
+export default function WebApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<ProductsList />}>
+            </Route>
+        </Routes>
+    </BrowserRouter>
     </div>
   );
 }
-
-export default App;
