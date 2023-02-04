3101005e24b200ed6b006746e75d551b63b8f66a
Tomiwa Ademidun <tomiademidun@gmail.com>
2023-02-04 07:28:42 +0200

    update title and favicon

--- src/scenes/Products/EventsList.tsx
+++ src/scenes/Products/EventsList.tsx
@@ -6,7 +6,7 @@ import EventCard from './EventCard'
 function EventsList() {
   return (
     <div className='EventsList container card shadow my-5 p-5'>
-        <h1 className='text-center mb-3'>Events List</h1>
+        <h1 className='text-center mb-3'>Events</h1>
         <div className="container">
 
           <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
