c78f4f0c0932549f8ac74116f64a4753babb46b2
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2023-02-26 22:55:58 -0700

    move set up into app

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,51 @@ import EventsList from "./scenes/Products/EventsList";
 import EventDetail from "./scenes/Products/EventDetail";
 import Header from "./components/Header/Header";
 import WalletCreate from "./scenes/Wallet/WalletCreate";
+import { Web3Button, Web3Modal } from "@web3modal/react";
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { goerli } from "wagmi/chains";
 
 export default function WebApp() {
+  const chains = [goerli];
+
+  // Wagmi client
+  const { provider } = configureChains(chains, [
+    walletConnectProvider({ projectId: "206fc16a8983532894a251baeafa4b0d" }),
+  ]);
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors: modalConnectors({
+      projectId: "206fc16a8983532894a251baeafa4b0d",
+      version: "1", // or "2"
+      appName: "web3Modal",
+      chains,
+    }),
+    provider,
+  });
+
+  // Web3Modal Ethereum Client
+  const ethereumClient = new EthereumClient(wagmiClient, chains);
   return (
     <div>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<EventsList />} />
-          <Route path="/wallet/create" element={<WalletCreate />} />
-          <Route path="/p/:eventId" element={<EventDetail />} />
-        </Routes>
-      </BrowserRouter>
+      <WagmiConfig client={wagmiClient}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<EventsList />} />
+            <Route path="/wallet/create" element={<WalletCreate />} />
+            <Route path="/p/:eventId" element={<EventDetail />} />
+          </Routes>
+        </BrowserRouter>
+      </WagmiConfig>
+      <Web3Modal
+        projectId="206fc16a8983532894a251baeafa4b0d"
+        ethereumClient={ethereumClient}
+      />
     </div>
   );
 }
--- src/scenes/Wallet/WalletCreate.tsx
+++ src/scenes/Wallet/WalletCreate.tsx
@@ -1,15 +1,7 @@
 import React, { useState } from "react";
 import { TextUtils } from "../../utils/TextUtils";
 import { TransactionUtils } from "../../utils/TransactionUtils";
-import { Web3Button, Web3Modal } from "@web3modal/react";
-import {
-  EthereumClient,
-  modalConnectors,
-  walletConnectProvider,
-} from "@web3modal/ethereum";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { goerli } from "wagmi/chains";
-
+import { Web3Button } from "@web3modal/react";
 
 function WalletCreate() {
   const [inputs, setInputs] = useState([
@@ -17,26 +9,6 @@ function WalletCreate() {
   ]);
   // usestate for threshold
   const [threshold, setThreshold] = useState(1);
-
-  const chains = [goerli];
-
-  // Wagmi client
-  const { provider } = configureChains(chains, [
-    walletConnectProvider({ projectId: "206fc16a8983532894a251baeafa4b0d" }),
-  ]);
-  const wagmiClient = createClient({
-    autoConnect: true,
-    connectors: modalConnectors({
-      projectId: "206fc16a8983532894a251baeafa4b0d",
-      version: "1", // or "2"
-      appName: "web3Modal",
-      chains,
-    }),
-    provider,
-  });
-
-  // Web3Modal Ethereum Client
-  const ethereumClient = new EthereumClient(wagmiClient, chains);
 
   const addInput = () => {
     setInputs([...inputs, { key: TextUtils.randomString(), value: "" }]);
@@ -72,59 +44,51 @@ function WalletCreate() {
   };
 
   return (
-    <>
-      <WagmiConfig client={wagmiClient}>
-        <div className="EventDetail container card shadow my-5 p-5">
-          <h1 className="text-center mb-3">Create a Wallet</h1>
-          <form>
-            {inputs.map((input, index) => (
-              <div key={input.key} className="form-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder={`Owner ${index + 1} Address`}
-                  value={input.value}
-                  onChange={(e) => handleInputChange(e, index)}
-                />
-                <button
-                  type="button"
-                  className="btn btn-outline-danger my-2"
-                  onClick={() => removeInput(input)}
-                >
-                  Remove
-                </button>
-              </div>
-            ))}
-            <button
-              type="button"
-              className="btn btn-outline-primary my-2"
-              onClick={addInput}
-            >
-              Add Another Owner
-            </button>
-            <div>
-              <hr />
-
-              <label>Owners needed to approve a transaction</label>
+      <div className="EventDetail container card shadow my-5 p-5">
+        <h1 className="text-center mb-3">Create a Wallet</h1>
+        <form>
+          {inputs.map((input, index) => (
+            <div key={input.key} className="form-group">
               <input
-                type="number"
+                type="text"
                 className="form-control"
-                value={threshold || inputs.length}
-                onChange={handleThresholdChange}
+                placeholder={`Owner ${index + 1} Address`}
+                value={input.value}
+                onChange={(e) => handleInputChange(e, index)}
               />
+              <button
+                type="button"
+                className="btn btn-outline-danger my-2"
+                onClick={() => removeInput(input)}
+              >
+                Remove
+              </button>
             </div>
-            <button className="btn btn-primary my-2" onClick={createWallet}>
-              Create Wallet
-            </button>
-            <Web3Button />
-          </form>
-        </div>
-      </WagmiConfig>
-      <Web3Modal
-        projectId="206fc16a8983532894a251baeafa4b0d"
-        ethereumClient={ethereumClient}
-      />
-    </>
+          ))}
+          <button
+            type="button"
+            className="btn btn-outline-primary my-2"
+            onClick={addInput}
+          >
+            Add Another Owner
+          </button>
+          <div>
+            <hr />
+
+            <label>Owners needed to approve a transaction</label>
+            <input
+              type="number"
+              className="form-control"
+              value={threshold || inputs.length}
+              onChange={handleThresholdChange}
+            />
+          </div>
+          <button className="btn btn-primary my-2" onClick={createWallet}>
+            Create Wallet
+          </button>
+          <Web3Button />
+        </form>
+      </div>
   );
 }
 
