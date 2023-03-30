1c583b14a7ef33f9b8ed5512d131658d4ca431ae
Tomiwa Ademidun <tomiademidun@gmail.com>
2023-03-30 23:12:12 +0200

    add prettier to wallet management file

--- src/scenes/Wallet/WalletFund.tsx
+++ src/scenes/Wallet/WalletFund.tsx
@@ -7,20 +7,20 @@ export interface WalletFundProps {
 
 function WalletFund() {
 
-  const [address, setAddress] = useState<string>(localStorage.getItem('safeAddress')||'');
+  const [address, setAddress] = useState<string>(localStorage.getItem('safeAddress') || '');
 
   function handleAddressChange(event: React.ChangeEvent<HTMLInputElement>) {
     setAddress(event.target.value);
   }
 
   const fundWallet = async () => {
-        
-    
+
+
     const safeOnRamp = await SafeOnRampKit.init(SafeOnRampProviderType.Stripe, {
       onRampProviderConfig: {
         // Get public key from Stripe: https://dashboard.stripe.com/register
         stripePublicKey:
-        'pk_test_51MZbmZKSn9ArdBimSyl5i8DqfcnlhyhJHD8bF2wKrGkpvNWyPvBAYtE211oHda0X3Ea1n4e9J9nh2JkpC7Sxm5a200Ug9ijfoO',
+          'pk_test_51MZbmZKSn9ArdBimSyl5i8DqfcnlhyhJHD8bF2wKrGkpvNWyPvBAYtE211oHda0X3Ea1n4e9J9nh2JkpC7Sxm5a200Ug9ijfoO',
         // Deploy your own server: https://github.com/safe-global/account-abstraction-sdk/tree/main/packages/onramp-kit/example/server
         onRampBackendUrl: 'https://aa-stripe.safe.global',
       },
@@ -40,17 +40,17 @@ function WalletFund() {
       }
     })
 
-    console.log({sessionData})
+    console.log({ sessionData })
   }
 
   return (
     <div id='stripe-root'>
       <label>Destination Address</label>
-            <input
-              className="form-control mb-3"
-              value={address}
-              onChange={handleAddressChange}
-            />
+      <input
+        className="form-control mb-3"
+        value={address}
+        onChange={handleAddressChange}
+      />
       <button className="btn btn-primary my-2" onClick={fundWallet}>
         Fund Wallet
       </button>
--- src/scenes/Wallet/WalletManage.tsx
+++ src/scenes/Wallet/WalletManage.tsx
@@ -26,31 +26,31 @@ function WalletManage() {
 
   return (
     <div className='TransactionManagement container card shadow my-5 p-5'>
-        <h1 className='text-center mb-3'>
-                Manage Wallet
-        </h1>
-        <ul className="nav nav-tabs" id="popupTab" role="tablist">
-          {steps.map((step, index) => {
-            return (
-              <li className="nav-item" role="presentation" key={index}>
-                <button className={`nav-link ${defaultTab === step.name ? 'active': ''}`} id={`${step.name}-tab`} data-bs-toggle="tab" data-bs-target={`#${step.name}`} type="button" role="tab" aria-controls={step.name} aria-selected="true" onClick={() => setDefaultTab(step.name as any)}>
-                  {step.title}
-                </button>
-              </li>
-            )
-          })
-          }
-        </ul>
-        <div className="p-3 tab-content" id="popupTabContent">
-          {steps.map((step, index) => {
-            return (
-              <div className={`tab-pane fade ${defaultTab === step.name ? 'show active': ''}`} id={step.name} role="tabpanel" aria-labelledby={`${step.name}-tab`} key={index}>
-                {step.component}
-              </div>
-            )
-          })
-          }
-        </div>
+      <h1 className='text-center mb-3'>
+        Manage Wallet
+      </h1>
+      <ul className="nav nav-tabs" id="popupTab" role="tablist">
+        {steps.map((step, index) => {
+          return (
+            <li className="nav-item" role="presentation" key={index}>
+              <button className={`nav-link ${defaultTab === step.name ? 'active' : ''}`} id={`${step.name}-tab`} data-bs-toggle="tab" data-bs-target={`#${step.name}`} type="button" role="tab" aria-controls={step.name} aria-selected="true" onClick={() => setDefaultTab(step.name as any)}>
+                {step.title}
+              </button>
+            </li>
+          )
+        })
+        }
+      </ul>
+      <div className="p-3 tab-content" id="popupTabContent">
+        {steps.map((step, index) => {
+          return (
+            <div className={`tab-pane fade ${defaultTab === step.name ? 'show active' : ''}`} id={step.name} role="tabpanel" aria-labelledby={`${step.name}-tab`} key={index}>
+              {step.component}
+            </div>
+          )
+        })
+        }
+      </div>
     </div>
   )
 }
